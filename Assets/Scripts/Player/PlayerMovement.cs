51ff8f9efe455f1407f9dfcf0f3788f8173a2767
Leonardo Fiedler <leonardo.fiedler.96@gmail.com>
2018-09-29 17:48:56 -0300

    Removing ignored files

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private float speed;
+	
+	
+	private void Awake() {
+		speed = 3;		
+	}
+	
+	void Update () {
+		float axisX = Input.GetAxis("Horizontal");
+		float axisY = Input.GetAxis("Vertical");
+		transform.Translate(new Vector3(axisX, axisY) * Time.deltaTime * speed);
+	}
+}
