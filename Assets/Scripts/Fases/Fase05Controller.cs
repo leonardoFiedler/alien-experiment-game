34ed818d2919ca2db738ebaeb1e7c18ae6c26fe3
Leonardo Fiedler <leonardo.fiedler.96@gmail.com>
2018-11-18 20:19:43 -0200

    Implementação de fase 05, implementação de boss final

---
+++ Assets/Scripts/Fases/Fase05Controller.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fase05Controller : BaseFaseController {
+
+    [SerializeField]
+    private EnemyCharacterController boss;
+
+	void Start () {
+        //Para testes somente da fase: Instancia o player
+        //Instancia o player que vai aparecer
+        //player = Instantiate(Resources.Load("Player", typeof(GameObject)), new Vector3(playerSpawn.position.x, playerSpawn.position.y, 0), Quaternion.identity) as GameObject;
+
+        //Para execucao normal, obtem o player
+        player = GameObject.FindGameObjectsWithTag("Player")[0];
+        player.transform.position = playerSpawn.position;
+    }
+	
+	public override void Update () {
+        base.Update();
+
+        CheckEndGame();
+    }
+
+    void CheckEndGame()
+    {
+        if (boss.Health.MyCurrentValue <= 0)
+        {
+            //EndGame
+            StartCoroutine(DeathKillCam());
+        }
+    }
+
+    //Controla a death kill cam do Boss - espera 2 segundos e direciona para a tela de vitoria
+    private IEnumerator DeathKillCam()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("Vitoria");
+    }
+}
